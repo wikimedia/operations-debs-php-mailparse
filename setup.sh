766c59be0478a9672c44930f322b066ff1f9cfd6
Mukunda Modell <mmodell@wikimedia.org>
2014-06-27 17:30:10 -0500

    initial commit

---
+++ setup.sh
@@ -0,0 +1,3 @@
+ssh twentyafterfour@gerrit.wikimedia.org -p 29418 'gerrit create-project' -d "Package php-mailparse" -n operations/debs/php-mailparse -o ops -p operations/debs
+#git init
+#git remote add gerrit ssh://twentyafterfour@gerrit.wikimedia.org:29418/operations/debs/php-mailparse
